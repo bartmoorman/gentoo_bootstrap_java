ee6d72511b394770bf25194f700929fb296b7f3e
Eric Dowd <edowd@dowdandassociates.com>
2013-04-11 09:12:57 +0000

    Switched from logback to blitz4j

--- gentoo-console-bootstrap/src/main/java/com/dowdandassociates/gentoo/console/bootstrap/Main.java
+++ gentoo-console-bootstrap/src/main/java/com/dowdandassociates/gentoo/console/bootstrap/Main.java
@@ -4,18 +4,38 @@ package com.dowdandassociates.gentoo.console.bootstrap;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import com.netflix.blitz4j.LoggingConfiguration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.dowdandassociates.gentoo.bootstrap.Amd64MinimalBootstrapModule;
 import com.dowdandassociates.gentoo.bootstrap.Bootstrapper;
 
 public class Main
 {
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args)
     {
-        Injector injector = Guice.createInjector(new Amd64MinimalBootstrapModule());
-        
-        Bootstrapper bootstrapper = injector.getInstance(Bootstrapper.class);
+        LoggingConfiguration.getInstance().configure();
 
-        bootstrapper.execute();
+        try
+        {
+            Injector injector = Guice.createInjector(new Amd64MinimalBootstrapModule());
+        
+            Bootstrapper bootstrapper = injector.getInstance(Bootstrapper.class);
+
+            bootstrapper.execute();
+        }
+        catch (Throwable t)
+        {
+            log.error(t.getMessage(), t);
+        }
+        finally
+        {
+            LoggingConfiguration.getInstance().stop();
+        }
     }
 }
 
