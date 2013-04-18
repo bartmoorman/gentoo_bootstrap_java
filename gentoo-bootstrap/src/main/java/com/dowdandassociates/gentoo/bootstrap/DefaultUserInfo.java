d0c8c56c71534823238291e8d6848480b8f14643
Eric Dowd <edowd@dowdandassociates.com>
2013-04-18 22:21:54 +0000

    DefaultUserInfo now using dynamic configuation

--- gentoo-bootstrap/src/main/java/com/dowdandassociates/gentoo/bootstrap/DefaultUserInfo.java
+++ gentoo-bootstrap/src/main/java/com/dowdandassociates/gentoo/bootstrap/DefaultUserInfo.java
@@ -1,6 +1,9 @@
 
 package com.dowdandassociates.gentoo.bootstrap;
 
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+
 import com.jcraft.jsch.UserInfo;
 
 import com.netflix.governator.annotations.Configuration;
@@ -15,37 +18,37 @@ public class DefaultUserInfo implements UserInfo
     private static Logger log = LoggerFactory.getLogger(DefaultUserInfo.class);
 
     @Configuration("com.dowdandassociates.gentoo.bootstrap.UserInfo.passphrase")
-    private String passphrase = null;
+    private Supplier<String> passphrase = Suppliers.ofInstance(null);
 
     @Configuration("com.dowdandassociates.gentoo.bootstrap.UserInfo.password")
-    private String password = null;
+    private Supplier<String> password = Suppliers.ofInstance(null);
 
     @Configuration("com.dowdandassociates.gentoo.bootstrap.UserInfo.yesNo")
-    private boolean yesNo = true;
+    private Supplier<Boolean> yesNo = Suppliers.ofInstance(true);
 
     public String getPassphrase()
     {
-        return passphrase;
+        return passphrase.get();
     }
 
     public String getPassword()
     {
-        return password;
+        return password.get();
     }
 
     public boolean promptPassphrase(String message)
     {
-        return (null != passphrase);
+        return (null != getPassphrase());
     }
 
     public boolean promptPassword(String message)
     {
-        return (null != password);
+        return (null != getPassword());
     }
 
     public boolean promptYesNo(String message)
     {
-        return yesNo;
+        return yesNo.get();
     }
 
     public void showMessage(String message)
