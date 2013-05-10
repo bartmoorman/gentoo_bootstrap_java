2f001e85a7e8e65f11ece7c73018b92c1418b1d5
Eric Dowd <edowd@dowdandassociates.com>
2013-05-10 05:53:54 +0000

    Create a new instance and a volume from a known good snapshot

---
+++ gentoo-bootstrap/src/main/java/com/dowdandassociates/gentoo/bootstrap/SnapshotOnDemandBootstrapInstanceInformationProvider.java
@@ -0,0 +1,105 @@
+
+package com.dowdandassociates.gentoo.bootstrap;
+
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.model.CreateVolumeRequest;
+import com.amazonaws.services.ec2.model.CreateVolumeResult;
+import com.amazonaws.services.ec2.model.DescribeVolumesRequest;
+import com.amazonaws.services.ec2.model.DescribeVolumesResult;
+import com.amazonaws.services.ec2.model.Image;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Volume;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.name.Named;
+
+import com.netflix.governator.annotations.Configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SnapshotOnDemandBootstrapInstanceInformationProvider extends AbstractOnDemandBootstrapInstanceInformationProvider
+{
+    private static Logger log = LoggerFactory.getLogger(SnapshotOnDemandBootstrapInstanceInformationProvider.class);
+
+    @Configuration("com.dowdandassociates.gentoo.bootstrap.BootstrapInstance.snapshotId")
+    private Supplier<String> snapshotId = Suppliers.ofInstance(null);
+
+    @Configuration("com.dowdandassociates.gentoo.bootstrap.BootstrapInstance.checkVolumeSleep")
+    private Supplier<Long> sleep = Suppliers.ofInstance(10000L);
+
+    @Inject
+    public SnapshotOnDemandBootstrapInstanceInformationProvider(
+            AmazonEC2 ec2Client,
+            @Named("Bootstrap Image") Optional<Image> bootstrapImage,
+            KeyPairInformation keyPairInformation,
+            SecurityGroupInformation securityGroupInformation,
+            BlockDeviceInformation blockDeviceInformation)
+    {
+        super(ec2Client, bootstrapImage, keyPairInformation, securityGroupInformation, blockDeviceInformation);
+    }
+
+    @Override
+    protected Optional<Volume> generateVolume(Optional<Instance> instance)
+    {
+        if (!instance.isPresent())
+        {
+            log.error("Instance is not present");
+            return Optional.absent();
+        }
+
+        if (null == snapshotId.get())
+        {
+            log.error("SnapshotId not set");
+            return Optional.absent();
+        }
+
+        log.info("AvailabilityZone=" + instance.get().getPlacement().getAvailabilityZone());
+        log.info("SnapshotId = " + snapshotId.get().toString());
+
+        CreateVolumeResult createVolumeResult = getEc2Client().createVolume(new CreateVolumeRequest().
+                withAvailabilityZone(instance.get().getPlacement().getAvailabilityZone()).
+                withSnapshotId(snapshotId.get()));
+
+        DescribeVolumesRequest describeVolumesRequest = new DescribeVolumesRequest().
+                withVolumeIds(createVolumeResult.getVolume().getVolumeId());
+
+        try
+        {
+            while (true)
+            {
+                log.info("Sleeping for " + sleep.get().toString() + " ms");
+                Thread.sleep(sleep.get());
+                DescribeVolumesResult describeVolumesResult = getEc2Client().describeVolumes(describeVolumesRequest);
+
+                Volume volume = describeVolumesResult.getVolumes().get(0);
+                String state = volume.getState();
+
+                log.info("volume state = " +  state);
+
+                if ("creating".equals(state))
+                {
+                    continue;
+                }
+
+                if (!"available".equals(state))
+                {
+                    return Optional.absent();
+                }
+
+                return Optional.fromNullable(volume);
+            }
+
+        }
+        catch (InterruptedException e)
+        {
+            return Optional.absent();
+        }
+    }
+}
+
